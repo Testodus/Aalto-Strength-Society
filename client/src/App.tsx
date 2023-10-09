dcb199b54965df3e1b8d13d5df8f28d9accd0765
rosannamanninen <rosannamanninen@outlook.com>
2023-10-09 21:50:48 +0300

    build the basic frame

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,78 @@
 import React from 'react';
-import './App.css';
 import NavBarRouter from './router/main';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+
+const AppHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding: 2rem 15% 2rem 15%;
+  align-items: center;
+
+  @media only screen and (max-width: 640px) {
+    padding: 0rem;
+    flex-direction: column;
+  }
+
+  svg,
+  h1 {
+    margin: 1rem;
+  }
+`;
+
+const AppFooter = styled.footer`
+  background: black;
+`;
+
+const ImageContainer = styled.svg`
+  height: 100px;
+  width: 100px;
+  color: grey;
+  background: grey;
+  flex-shrink: 0;
+`;
+
+const MainHeading = styled.h1`
+  font-size: 45px;
+  text-align: center;
+
+  @media only screen and (max-width: 465px) {
+    font-size: 70px;
+  }
+`;
 
 function App() {
+  const [screenSize, setScreenSize] = useState(getCurrentDimension());
+
+  function getCurrentDimension() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  useEffect(() => {
+    const updateDimension = () => {
+      setScreenSize(getCurrentDimension());
+    };
+    window.addEventListener('resize', updateDimension);
+
+    return () => {
+      window.removeEventListener('resize', updateDimension);
+    };
+  }, [screenSize]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Aalto Strength Society</h1>
-      </header>
+    <div>
+      <AppHeader>
+        <ImageContainer></ImageContainer>
+        <MainHeading>
+          {screenSize.width > 465 ? 'Aalto Strength Society' : 'ASS'}
+        </MainHeading>
+      </AppHeader>
       <NavBarRouter />
+      <AppFooter></AppFooter>
     </div>
   );
 }
