35b75f5a1ceb851f6533c56a3d4c3760ef296ace
rosannamanninen <rosannamanninen@outlook.com>
2023-10-09 21:51:14 +0300

    add navbar and router

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,23 +1,116 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useState } from 'react';
 
-export const Nav = styled.nav`
-  background: #63d471;
-  height: 85px;
+const NavbarContainer = styled.nav`
+  max-width: 1000px;
+  width: 100%;
+  height: auto;
   display: flex;
-  justify-content: space-between;
-  padding: 0.2rem calc((100vw - 1000px) / 2);
-  z-index: 12;
-  /* Third Nav */
-  /* justify-content: flex-start; */
+  justify-content: center;
+  flex-wrap: wrap;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    padding: 1rem 1rem 1rem 1rem;
+    font-size: 30px;
+
+    @media only screen and (max-width: 640px) {
+      font-size: 26px;
+    }
+  }
+`;
+
+const FullWidthDiv = styled.div`
+  width: 100%;
+  max-width: 750px;
+  margin: 0;
+  padding: 0;
+`;
+
+const SecondaryNavbarContainer = styled.nav`
+  max-width: 1000px;
+  widht: max-content
+  height: auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    padding: 1rem 1rem 1rem 1rem;
+    font-size: 24px;
+
+    @media only screen and (max-width: 640px) {
+      font-size: 22px;
+    }
+  }
+`;
+
+const NavContainer = styled.div`
+  width: 100%;
+  background: #e7e7e7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (min-width: 640px) {
+    flex-direction: row;
+  }
+`;
+
+const NavButton = styled.button`
+  text-decoration: none;
+  font-weight: bold;
+  border: none;
+  font-size: 20px;
+  background: white;
+  padding: 1rem;
+  max-width: 8rem;
+  margin: 0.5rem 2rem 0.5rem 2rem;
+
+  @media only screen and (min-width: 640px) {
+    display: none;
+    widht: 0;
+    height: 0;
+  }
 `;
 
 const Navbar = () => {
+  const [menuBarOpen, setMenubarOpen] = useState(window.innerWidth < 640);
+  const [secondaryMenuOpen, setSecondaryMenuOpen] = useState(false);
+
+  const CloseMenuBar = () => {
+    setMenubarOpen(!menuBarOpen);
+  };
+
+  const CloseAboutUsMenu = () => {
+    setSecondaryMenuOpen(!secondaryMenuOpen);
+  };
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </div>
+    <NavContainer>
+      <NavButton onClick={CloseMenuBar}> MENU </NavButton>
+      {menuBarOpen || window.innerWidth > 640 ? (
+        <FullWidthDiv>
+          <NavbarContainer>
+            <Link to="/">HOME</Link>
+            <Link to="/contact">CONTACT</Link>
+            <a onClick={CloseAboutUsMenu}>ABOUT US</a>
+          </NavbarContainer>
+          {secondaryMenuOpen ? (
+            <SecondaryNavbarContainer>
+              <Link to="/board">Board</Link>
+              <Link to="/rules">Rules</Link>
+              <Link to="/join">How to join?</Link>
+              <Link to="/harassment">Harassment situations</Link>
+            </SecondaryNavbarContainer>
+          ) : null}
+        </FullWidthDiv>
+      ) : null}
+    </NavContainer>
   );
 };
 
--- client/src/router/main.tsx
+++ client/src/router/main.tsx
@@ -1,17 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import HomePage from '../pages/HomePage';
-import About from '../pages/about/About';
+import Board from '../pages/board/Board';
+import styled from 'styled-components';
+import Contact from '../pages/contact/Contact';
+import Rules from '../pages/rules/Rules';
+import Join from '../pages/join/Join';
+import Harassment from '../pages/harassment/Harassment';
+
+const FrameDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  padding: 3rem 0rem 3rem 0rem;
+
+  @media only screen and (max-width: 640px) {
+    padding: 2rem 0rem 2rem 0rem;
+  }
+`;
 
 const NavBarRouter = () => {
   return (
     <div>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" Component={HomePage} />
-          <Route path="/about" Component={About} />
-        </Routes>
+        <FrameDiv>
+          <Routes>
+            <Route path="/" Component={HomePage} />
+            <Route path="/board" Component={Board} />
+            <Route path="/contact" Component={Contact} />
+            <Route path="/rules" Component={Rules} />
+            <Route path="/join" Component={Join} />
+            <Route path="/harassment" Component={Harassment} />
+          </Routes>
+        </FrameDiv>
       </Router>
     </div>
   );
