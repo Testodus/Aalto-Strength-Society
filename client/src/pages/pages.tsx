f08702659a42d02279f00476986aeafc658172db
rosannamanninen <rosannamanninen@outlook.com>
2023-10-11 14:16:44 +0300

    shared export for pages

---
+++ client/src/pages/pages.tsx
@@ -0,0 +1,23 @@
+import Board from './board/Board';
+import ErrorElement from './error/Error';
+import Join from './join/Join';
+import Harassment from './harassment/Harassment';
+import Login from './loginpage/Login';
+import Register from './register/Register';
+import Rules from './rules/Rules';
+import ProfileElement from './profile/Profile';
+import HomePage from './HomePage';
+import Noticeboard from './noticeboard/Noticeboard';
+
+export {
+  Board,
+  ErrorElement,
+  Join,
+  Harassment,
+  Login,
+  Register,
+  Rules,
+  ProfileElement,
+  HomePage,
+  Noticeboard,
+};
