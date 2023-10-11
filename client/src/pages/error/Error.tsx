19b6286eb39fe549de332fad70ab652684252fcc
rosannamanninen <rosannamanninen@outlook.com>
2023-10-11 14:18:31 +0300

    errorpage base

---
+++ client/src/pages/error/Error.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BasicFrame from '../components/BasicFrame';
+
+const ErrorElement = () => {
+  return <BasicFrame topic={'harassment'}></BasicFrame>;
+};
+
+export default ErrorElement;
