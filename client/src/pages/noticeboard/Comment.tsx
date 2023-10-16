d0a050c440eccb12600b368203c6cb26d1bed1b6
rosannamanninen <rosannamanninen@outlook.com>
2023-10-16 15:57:16 +0300

    add comments

---
+++ client/src/pages/noticeboard/Comment.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import { DetailText, DarkBodyText } from '../../shared-styles';
+import { Link } from 'react-router-dom';
+import { DummyProfiles } from '../../assets/noticeBoardDummy';
+import { Comment } from '../../types';
+
+const CommentDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 1rem;
+  background: #f0c7ad;
+  padding: 1rem;
+  justify-content: space-between;
+  height: min-content;
+  margin: 0.5rem;
+
+  p,
+  a {
+    margin: 0.2rem;
+  }
+`;
+
+type CommentProps = {
+  comment: Comment;
+};
+
+/**
+ *Can be used as the full Notice with comments and all with FullNotice: true and as a teaser with FullNotice:false
+ * @returns Notice -element
+ */
+const CommentEl = ({ comment }: CommentProps) => {
+  const getUsername = (id: string) => {
+    const user = DummyProfiles.find(profile => profile.userID === id);
+    return user ? user.username : 'did not find it';
+  };
+
+  const addZeroToTime = (number: number) =>
+    number > 9 ? number : '0' + number;
+
+  const getDate = (timeStamp: number) => {
+    const date = new Date(timeStamp);
+    return (
+      addZeroToTime(date.getHours()) +
+      ':' +
+      addZeroToTime(date.getMinutes()) +
+      ', ' +
+      date.toDateString()
+    );
+  };
+
+  return (
+    <CommentDiv>
+      <DetailText>
+        <Link to={'/profile/' + comment.userID}>
+          {getUsername(comment.userID)}
+        </Link>{' '}
+        {getDate(comment.timeStamp)}
+      </DetailText>
+      <DarkBodyText>{comment.comment}</DarkBodyText>
+    </CommentDiv>
+  );
+};
+
+export default CommentEl;
