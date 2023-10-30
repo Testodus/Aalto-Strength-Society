8770cc33dea2b1e31635076993c41402854f47ac
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-10-30 13:07:51 +0200

    add proper path to .env (in the root of server folder) for dotenv and change tsconfig

--- server/database/util/config.ts
+++ server/database/util/config.ts
@@ -1,8 +1,9 @@
+// This file offers the value for the database url
 import dotenv from 'dotenv';
-dotenv.config();
+// config() function loads the DATABASE_URL value from .env file to process.env
+// This is to prevent setting process.env values every time to terminal sessions
+dotenv.config({ path: '../../.env' });
 
 // This is quite dumb but util.ts will catch the error :D Open to improvements
 export const DATABASE_URL: string =
   process.env.DATABASE_URL || 'no database URL';
-export const PORT: string | number = process.env.PORT || 3001;
-export const SECRET: string | undefined = process.env.SECRET;
